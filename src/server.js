f03833dd4c6eb90caa177619cdd1d68a752eb62d
rsl150101 <dnwnd501@naver.com>
2023-01-11 17:34:45 +0900

    0.3. 프론트엔드 세팅 뷰 엔진 pug 설정, views 경로 지정, static 폴더 지정, 프론트엔드 js 파일 nodemon 감시 제외

--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+//* 뷰 엔진 설정
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views") // views 경로 설정
+
+//* static 폴더 지정 
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/",(req,res)=>{
+    return res.render("home")
+})
+
 app.listen(PORT,()=>{
     console.log(`✅ 서버가 연결되었습니다. http://localhost:${PORT}`)
 })
