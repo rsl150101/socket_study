dd09d5466fa6dfc256a94e60b11c862f95bbd55d
rsl150101 <dnwnd501@naver.com>
2023-01-14 02:40:49 +0900

    1.6. 메시지 비교 처리를 위한 프론트 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,22 +1,49 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message-form");
+const nicknameForm = document.querySelector("#nickname-form");
+
 //* frontend 에서 backend 연결
-const socket = new WebSocket(`ws://${window.location.host}`) //- 여기서 socket은 서버로의 연결을 뜻함
+const socket = new WebSocket(`ws://${window.location.host}`); //- 여기서 socket은 서버로의 연결을 뜻함
+
+//* 메시지를 JSON 형태의 String 으로 보내기 위해 변환해주는 함수
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  //- 단순한 문자열로 메시지를 전송했을 때 구분할 수 없었던 점을 개선하기 위해 JSON 형태로 작성하고
+  //- type 에 값을 할당하여 구분해준다.
+  return JSON.stringify(msg);
+}
 
 //* 서버로 socket 연결이 되었을 때
-socket.addEventListener("open",()=>{
-    console.log("✅ WebSocket : 브라우저가 서버에 연결되었습니다.")
-})
+socket.addEventListener("open", () => {
+  console.log("✅ WebSocket : 브라우저가 서버에 연결되었습니다.");
+});
 
 //* 서버로부터 전달된 message 이벤트를 받았을 때
-socket.addEventListener("message",(message)=>{ //- 서버에서 send 로 보낸 message 이벤트와 data를 받아옴
-    console.log(`서버로부터 전달된 메시지 : `,message.data)
-})
+socket.addEventListener("message", (message) => {
+  //- 서버에서 send 로 보낸 message 이벤트와 data를 받아옴
+  const li = document.createElement("li");
+  li.textContent = message.data;
+  messageList.prepend(li);
+});
 
 //* 서버로 socket 연결이 해제되었을 때
-socket.addEventListener("close",()=>{
-    console.log("❌ WebSocket : 서버와 연결이 해제되었습니다.")
-})
+socket.addEventListener("close", () => {
+  console.log("❌ WebSocket : 서버와 연결이 해제되었습니다.");
+});
 
-setTimeout(()=>{
-    socket.send("이 메시지는 브라우저로부터 전달됨")
-},5000
-)
+//* event handle function
+function handleMessageSubmit(event) {
+  event.preventDefault(); //- 기본 이벤트 막음
+  const messageInput = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", messageInput.value));
+  messageInput.value = "";
+}
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const nicknameInput = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", nicknameInput.value));
+}
+
+//* DOM event
+messageForm.addEventListener("submit", handleMessageSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -6,45 +6,52 @@ const app = express();
 const PORT = 3000;
 
 //* 뷰 엔진 설정
-app.set("view engine", "pug")
-app.set("views", __dirname + "/views") //- views 경로 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); //- views 경로 설정
 
 //* static 폴더 지정
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/",(req,res)=>{
-    return res.render("home")
-})
+app.get("/", (req, res) => {
+  return res.render("home");
+});
 
-const handleListen = ()=>{
-    console.log(`✅ HTTP 서버가 연결되었습니다. http://localhost:${PORT}`)
-}
+const handleListen = () => {
+  console.log(`✅ HTTP 서버가 연결되었습니다. http://localhost:${PORT}`);
+};
 
 //* 기존의 서버 구축
 //// app.listen(PORT,handleListen)
 
 //* WebSocket 를 사용하기 위해 서버 생성
-const server = http.createServer(app)
+const server = http.createServer(app);
 //- 기존의 app.listen 은 생성과 실행이 같이 되어 서버에 접근할 수 없었는데 http 메서드를 통해 서버를 생성하여 서버에 접근할 수 있음
 
 //* WebSocket server
-const wsSever = new WebSocket.Server({server})
+const wsSever = new WebSocket.Server({ server });
 //- http 서버와 WebSocket 서버를 같이 사용함
 //- ws.Server({server : WebSocket 에 연결할 HTTP 서버, port : WebSocket 연결시 사용할 port 로 생략시 server 에 연결된 HTTP 서버의 포트를 사용 })
 //! 항상 http 서버와 WebSocket 서버를 같은 port 로 함께 만들지 않아도 됨
 
+//* 브라우저가 연결된건 알지만 몇 명이 연결되었는진 알 수 없어 가짜 socket 데이터 베이스로 구현
+const sockets = []; //- socket 이 연결될 때 마다 담길 배열
+
 //* WebSocket Connection Event
-wsSever.on("connection",(socket)=>{ //- 매개변수 socket 은 연결된 브라우저를 뜻함
-    console.log("✅ WebSocket : 서버가 브라우저에 연결되었습니다.")
-    socket.on("close", ()=>{ //- 브라우저와 socket 연결이 해제되었을 때 이벤트
-        console.log("❌ WebSocket : 브라우저와 연결이 해제되었습니다.")
-    })
-    socket.send("이 메시지는 서버로부터 전달됨") //- 브라우저로 message 이벤트와 함께 Hello 를 전달
-    socket.on("message",(message)=>{
-        console.log(`브라우저로부터 전달된 메시지 : `,message.toString())
-    })
-})
+wsSever.on("connection", (socket) => {
+  //- 매개변수 socket 은 연결된 브라우저를 뜻함
+  sockets.push(socket); //- 가짜 socket 데이터베이스에 추가
+  console.log("✅ WebSocket : 서버가 브라우저에 연결되었습니다.");
+  socket.on("close", () => {
+    //- 브라우저와 socket 연결이 해제되었을 때 이벤트
+    console.log("❌ WebSocket : 브라우저와 연결이 해제되었습니다.");
+  });
+  //socket.send("이 메시지는 서버로부터 전달됨"); //- send 는 브라우저로 message 이벤트와 함께 내용을 전달
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    console.log(`브라우저로부터 전달된 메시지 : `, message.toString());
+  });
+});
 //- on method 는 backend 에 연결된 사람의 정보(socket)를 주는데 callback 함수로 정보(socket)를 받음
 
 //* WebSocket 위한 서버 구축
-server.listen(PORT,handleListen)
+server.listen(PORT, handleListen);
