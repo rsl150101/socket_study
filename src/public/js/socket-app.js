b5fc50895f5daf44dfc01cd8ccab5795e8c0ecc8
rsl150101 <dnwnd501@naver.com>
2023-01-15 18:27:50 +0900

    2.1. socket.io 설정 패키지 추가 : socket.io nodemon 설정 : 실행할 파일 server.js -> socketio-server.js 수정 새로운 파일로 템플릿, 프론트, 백 생성 후 socket.io 를 이용해서 연결 구현

---
+++ src/public/js/socket-app.js
@@ -0,0 +1,3 @@
+//* 서버로 socket.io 연결
+const socket = io(); //- 템플릿에서 이미 설치했기 때문에 socket.io 함수를 바로 사용 가능함
+//- io function 은 알아서 socket.io 를 실행하고 있는 서버를 찾음
---
+++ src/socketio-server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+
+const app = express();
+const PORT = 3000;
+
+//* 뷰 엔진 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); //- views 경로 설정
+
+//* static 폴더 지정
+app.use("/public", express.static(__dirname + "/public"));
+
+//* route API
+app.get("/", (req, res) => {
+  return res.render("socket-home");
+});
+
+//* Listen handle function
+const handleListen = () => {
+  console.log(`✅ HTTP 서버가 연결되었습니다. http://localhost:${PORT}`);
+};
+
+//* 서버 생성
+const server = http.createServer(app);
+
+//* socket.io 서버 생성
+const io = SocketIO(server);
+
+//* socket.io 서버 연결
+io.on("connection", (socket) => {
+  console.log(socket);
+});
+
+//* socket.io 위한 서버 구축
+server.listen(PORT, handleListen);
